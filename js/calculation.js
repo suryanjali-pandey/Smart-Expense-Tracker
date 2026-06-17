e45aa8fa8b62ec61d69f44050200bcdf2d064a8c
dipanjanmaity890 <dipanjanmaity890@gmail.com>
2026-06-17 15:28:57 +0530

    Add calculation functions

---
+++ js/calculation.js
@@ -0,0 +1,15 @@
+export const calculateIncome = (transactions) =>
+  transactions
+    .filter((transaction) => transaction.type === "income")
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+export const calculateExpense = (transactions) =>
+  transactions
+    .filter((transaction) => transaction.type === "expense")
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+export const calculateBalance = (transactions) =>
+  calculateIncome(transactions) - calculateExpense(transactions);
+
+
+// diya-------------------
